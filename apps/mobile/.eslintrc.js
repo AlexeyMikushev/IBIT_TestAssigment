4f78b90670b32286170fe094cd4d22d3ec33ee09
alexei.mikushev <alexei.mikushev@gmail.com>
2026-07-10 18:01:58 +0300

    fix(mobile): resolve node_modules paths

--- apps/mobile/.eslintrc.js
+++ apps/mobile/.eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      files: ['metro.config.js', 'jest.config.js', 'babel.config.js'],
+      parserOptions: { requireConfigFile: false },
+    },
+  ],
 };
