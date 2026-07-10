cb8b6d680024c2d298759c9450341ab918576361
alexei.mikushev <alexei.mikushev@gmail.com>
2026-07-10 22:37:06 +0300

    feat(general): add react-native-web

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: /^react-native$/, replacement: 'react-native-web' }],
+    extensions: [
+      '.web.tsx',
+      '.web.ts',
+      '.web.jsx',
+      '.web.js',
+      '.tsx',
+      '.ts',
+      '.jsx',
+      '.js',
+    ],
+  },
+  define: {
+    __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
+    global: 'globalThis',
+  },
 })
